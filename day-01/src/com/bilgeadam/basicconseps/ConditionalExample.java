dcf52dac59f3f7c520e9cb5b46781d501874b12a
kmsync <tommyjwi@gmail.com>
2022-07-25 06:07:42 +0300

    Conditional example has been done.

--- day-01/src/com/bilgeadam/basicconseps/ConditionalExample.java
+++ day-01/src/com/bilgeadam/basicconseps/ConditionalExample.java
@@ -11,5 +11,25 @@ public class ConditionalExample {
         System.out.println(a != b);
         System.out.println(a > b);
         System.out.println(a < b);
+
+        System.out.println("---If");
+
+        String city = "ankara";
+        int c = 15;
+
+        // And Conditional
+        if (city == "ankara" && c==15) {
+            System.out.println("Inside if section..");
+        //    if (c == 115) {
+        //        System.out.println("c : " + c);
+        //    }
+        // OR Conditional
+        //if (city == "ankara" || c==15) {
+        //        System.out.println("Inside if section..");
+        } else if (city == "Ankara") {
+            System.out.println("Inside other if section..");
+        } else {
+            System.out.println("Inside else section..");
+        }
     }
 }
