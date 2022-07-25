01d14a138a447ad6a976a54c0a4f3afdf6a1ef9d
kmsync <tommyjwi@gmail.com>
2022-07-25 06:25:13 +0300

    Different One Conditional Example Has Been Done.

---
+++ day-01/src/com/bilgeadam/basicconseps/ConditionalExampleV2.java
@@ -0,0 +1,35 @@
+package com.bilgeadam.basicconseps;
+
+import java.util.Scanner;
+
+public class ConditionalExampleV2 {
+
+    public static void main(String[] args) {
+
+        System.out.println("---BMI Calculator");
+
+        Scanner scanner = new Scanner(System.in);
+
+        double weight;
+        double height;
+        double result;
+
+        System.out.println("Enter your weight?");
+        weight = scanner.nextDouble();
+        System.out.println("Enter your height?");
+        height = scanner.nextDouble();
+        //result = weight / (height * height);
+        result = weight / Math.pow(height, 2);
+
+        String preStr = "You are ";
+        if (result < 18.5) {
+            System.out.println(preStr + "UNDERWEIGHT");
+        } else if (result >= 18.5 && result < 25.0) {
+            System.out.println(preStr + "NORMAL");
+        } else if (result >= 25.0 && result < 30.0) {
+            System.out.println(preStr + "OVERWEIGHT");
+        } else if (result >= 30) {
+            System.out.println(preStr + "VERY OVERWEIGHT");
+        }
+    }
+}
